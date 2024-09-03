0f5f64eeeb8ee729f7fd77586c0022f2494bf18a
AgusSanticchia <santicchiaagus85@gmail.com.ar>
2024-09-03 11:30:55 -0300

    Arreglos en el post de cuentas

--- src/main/java/ar/edu/utn/frbb/tup/model/exception/EdadInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.utn.frbb.tup.model.exception;
-
-public class EdadInvalidaException extends RuntimeException {
-    public EdadInvalidaException(String message) {
-        super(message);
-    }
-}
