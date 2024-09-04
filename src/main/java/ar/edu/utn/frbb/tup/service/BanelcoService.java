44c4e04dc5001dfc9c31a89c1429a6813c1f7de3
AgusSanticchia <santicchiaagus85@gmail.com.ar>
2024-09-04 20:33:32 -0300

    desarrollo banelcoService

--- src/main/java/ar/edu/utn/frbb/tup/service/BanelcoService.java
+++ src/main/java/ar/edu/utn/frbb/tup/service/BanelcoService.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Service;
 import java.util.Random;
 
 @Service
-public class BanelcoService {
-    Random r = new Random();
+public class BanelcoService throws CuentaNoPermitaException {
+
+    int random = (int) ((Math.random() * (10 - 1)) + 1);
+    String message;
+    if (random <= 2) {
+      throw new CuentaNoPermitaException("La cuenta no permite realizar transferencias");
+    } else {
+        message = "Transferencia realizada con éxito";
+    }
 }
