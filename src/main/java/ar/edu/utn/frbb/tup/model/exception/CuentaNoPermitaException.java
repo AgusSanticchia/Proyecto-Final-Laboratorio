1c13a27f230ff2383aa2cff906dbcaa04de6c5e5
AgusSanticchia <santicchiaagus85@gmail.com.ar>
2024-09-04 21:10:42 -0300

    creacion de exceptions y banelco dto

---
+++ src/main/java/ar/edu/utn/frbb/tup/model/exception/CuentaNoPermitaException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frbb.tup.model.exception;
+
+public class CuentaNoPermitidaException extends RuntimeException {
+    public CuentaNoPermitidaException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ar/edu/utn/frbb/tup/service/BanelcoService.java
+++ src/main/java/ar/edu/utn/frbb/tup/service/BanelcoService.java
@@ -1,16 +1,4 @@
 package ar.edu.utn.frbb.tup.service;
 
-import org.springframework.stereotype.Service;
-import java.util.Random;
-
-@Service
-public class BanelcoService throws CuentaNoPermitaException {
-
-    int random = (int) ((Math.random() * (10 - 1)) + 1);
-    String message;
-    if (random <= 2) {
-      throw new CuentaNoPermitaException("La cuenta no permite realizar transferencias");
-    } else {
-        message = "Transferencia realizada con éxito";
-    }
+public class BanelcoService {
 }
